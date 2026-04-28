654fbcb5db2b680f3d1713213bf8a7ad2bf2918b
XSong1205 <songpeize050712@outlook.com>
2026-04-28 08:41:26 +0800

    fix: 修复 qq gif 动画表情只同步第一帧的问题 feat: 尝试支持 QQ 超级表情渲染

--- push.py
+++ push.py
@@ -1,13 +1,6 @@
 import os
-
-print("----------------------")
-print("| TQSync Auto Pusher |")
-print("----------------------")
-
-print("1.版本号处理")
-print("major,minor,patch")
+print("----------------------"); print("| TQSync Auto Pusher |"); print("----------------------"); print("1.版本号处理"); print("major,minor,patch")
 bump_type = input("what do u want to bump: ")
-
 if bump_type == "major":
     os.system("python ./utils/version_bumper.py -major")
 elif bump_type == "minor":
@@ -15,11 +8,9 @@ elif bump_type == "minor":
 elif bump_type == "patch":
     os.system("python ./utils/version_bumper.py -patch")
 else:
-    print("Invalid input")
-
+    print("未知输入内容，未做版本号更改")
 print("2.Git Commit")
 os.system("git add .")
 os.system("git commit ")
+print("3.Git Push")
 os.system("git push origin Next")
-
-
