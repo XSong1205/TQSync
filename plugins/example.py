1de362932c966fc1c1cc29f536801a48d7fd4be8
XSong1205 <songpeize050712@outlook.com>
2026-04-29 01:55:05 +0800

    docs: 新增插件开发文档，可前往 `/docs/plugin_development_guide` 查看 chore: 增加example插件 用于临时debug

--- plugins/example.py
+++ plugins/example.py
@@ -2,23 +2,23 @@ from core.plugin_base import PluginBase
 
 
 class ThumbsUp(PluginBase):
-    name = "ThumbsUp"
+    name = "example"
     version = "1.0"
-    description = "自动检测'赞我'消息并回复大拇指表情"
-    author = "TQSync"
+    description = "test"
+    author = "XSong"
 
     def get_matchers(self):
         return [
-            {"type": "exact", "pattern": "赞我", "description": "收到'赞我'后回复👍"},
+            {"type": "exact", "pattern": "/plugintest", "description": "测试plugin是否正常工作"},
         ]
 
     async def on_message(self, platform, user_id, group_id, message):
         if platform == 'tg':
             await self.ctx.tg_bot.send_message(
                 chat_id=group_id,
-                text="👍👍👍"
+                text="Example Plugin is working."
             )
         elif platform == 'qq':
-            await self.ctx.qq_client.send_group_msg(group_id, "👍👍👍")
+            await self.ctx.qq_client.send_group_msg(group_id, "Example Plugin is working.")
 
         return None
